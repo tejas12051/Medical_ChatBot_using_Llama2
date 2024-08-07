adc422d161d22b1ea3c9a8720f9536f5aa05c557
Tejas Patil <92673092+tejas12051@users.noreply.github.com>
2024-08-07 08:51:23 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name = 'Medical Chatbot',
     version= '0.0.0',
-    author= 'Boktiar Ahmed Bappy',
-    author_email= 'entbappy73@gmail.com',
+    author= 'Tejas Patil',
+    author_email= 'patiltejas12051@gmail.com',
     packages= find_packages(),
     install_requires = []
 
